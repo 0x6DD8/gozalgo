10917359f78fa813add4f854ed6d2abd743e1f06
0x6DD8 <root@lewd.red>
2022-04-12 00:32:28 +0200

    This

---
+++ go.mod
@@ -0,0 +1,4 @@
+module gozalgo
+
+
+go 1.18
---
+++ gozalgo.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func randInt(min int, max int) (x int) {
+	x = rand.Intn(max-min) + min
+	return x
+}
+
+func zalgo() (s string) {
+	s = fmt.Sprintf("%c", randInt(768, 879))
+	return s
+}
+
+func zalgoText(text string, curseCount int) {
+
+	tmpText := text[0:1]
+
+	for i := 0; i < len([]rune(text)); i++ {
+
+		tmpText = text[i : i+1]
+
+		var tmp string
+
+		for j := 0; j < curseCount; j++ {
+			tmp += zalgo()
+		}
+
+		tmpText = text[i:i+1] + tmp
+
+		fmt.Printf(tmpText)
+	}
+	fmt.Printf("\n")
+}
+
+func helpText() {
+	fmt.Println("Usage: gozalgo <text> <number of Cursed Characters> \n\n", "-h   : this")
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	arg := os.Args
+
+	legthOfArgs := len(arg)
+
+	switch legthOfArgs {
+	case 1:
+		fmt.Println("missing args, type 'gozalgo -h' for help")
+		break
+	case 2:
+		if strings.Contains(arg[1], "-h") {
+			helpText()
+		} else {
+			zalgoText(arg[1], 5)
+		}
+
+		break
+	case 3:
+		x, err := strconv.Atoi(arg[2])
+		if err != nil {
+			fmt.Println("error, type 'gozalgo -h' for help")
+			os.Exit(2)
+		}
+		zalgoText(arg[1], x)
+		break
+	default:
+		if legthOfArgs > 3 {
+			fmt.Println("error, type 'gozalgo -h' for help")
+		}
+		break
+	}
+
+}
